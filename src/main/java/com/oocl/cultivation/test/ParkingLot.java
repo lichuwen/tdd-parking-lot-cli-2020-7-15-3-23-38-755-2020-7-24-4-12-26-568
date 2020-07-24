3b02d57522746e10321dd7ebe9b4fbfbae06a353
lichuwen <545759585@qq.com>
2020-07-24 15:35:18 +0800

    [karen] feature:add Car,CarTicket,ParkingLot class

---
+++ src/main/java/com/oocl/cultivation/test/ParkingLot.java
@@ -0,0 +1,11 @@
+package com.oocl.cultivation.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParkingLot {
+    public CarTicket park(Car car){
+        return null;
+    }
+
+}
