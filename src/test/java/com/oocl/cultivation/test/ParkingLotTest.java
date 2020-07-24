6b4bb510e8312f1ea982dee824743005f99d4386
lichuwen <545759585@qq.com>
2020-07-24 15:45:57 +0800

    [karen] feature:add should_fetch_car_when_fetch_given_carTicket test

--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -16,4 +16,17 @@ public class ParkingLotTest {
         //then
         assertNotNull(carTicket);
     }
+
+    @Test
+    void should_fetch_car_when_fetch_given_carTicket(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        CarTicket carTicket = new CarTicket();
+
+        //when
+        Car car = parkingLot.fetch(carTicket);
+
+        //then
+        assertNotNull(car);
+    }
 }
