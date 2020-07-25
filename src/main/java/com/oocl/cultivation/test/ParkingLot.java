a2c48693cdae8c75a0fbb9219a24425f68383ff5
Lichuwen <lichuwen@CHUWENdeMacBook-Pro.local>
2020-07-25 20:11:56 +0800

    [story1] [karen] refactor:parkingLot's park function

--- src/main/java/com/oocl/cultivation/test/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/test/ParkingLot.java
@@ -4,9 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+
+    private int capacity;
     private Map<CarTicket,Car> parkLots = new HashMap<>();
+
+    public ParkingLot() {
+        this.capacity = 10;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Map<CarTicket, Car> getParkLots() {
+        return parkLots;
+    }
+
     public CarTicket park(Car car){
         CarTicket carTicket = new CarTicket();
+        if(parkLots.size() >= capacity){
+            return null;
+        }
         parkLots.put(carTicket,car);
         return carTicket;
     }
