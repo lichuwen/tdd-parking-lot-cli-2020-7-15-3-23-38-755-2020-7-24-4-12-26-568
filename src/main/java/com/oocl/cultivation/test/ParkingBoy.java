f5bc83fae0178b6ae6a2b43df278bbee8b22483f
Lichuwen <lichuwen@CHUWENdeMacBook-Pro.local>
2020-07-26 01:07:56 +0800

    [story2][karen] refactor:ParkingBoy's

--- src/main/java/com/oocl/cultivation/test/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/test/ParkingBoy.java
@@ -1,31 +1,45 @@
 package com.oocl.cultivation.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingBoy {
 
     private ParkingLot parkingLot;
-
     private String wrongMessage;
+    private Map<String,String> wrongMessages = new HashMap<>();
 
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
+        wrongMessages.put("unRecognize","Unrecognized parking ticket.");
+        wrongMessages.put("unProvide","Please provide your parking ticket.");
+        wrongMessages.put("noPosition","Not enough position.");
     }
 
     public String getWrongMessage() {
         return wrongMessage;
     }
 
+    public Map<String, String> getWrongMessages() {
+        return wrongMessages;
+    }
+
     public CarTicket park(Car car) {
         CarTicket ticket = parkingLot.store(car);
         if (ticket == null) {
-            wrongMessage = "Unrecognized parking ticket.";
+            wrongMessage = wrongMessages.get("unProvide");
         }
         return ticket;
     }
 
     public Car fetch(CarTicket ticket) {
+        if(ticket == null){
+            wrongMessage = wrongMessages.get("unProvide");
+            return null;
+        }
         Car car = parkingLot.move(ticket);
         if (car == null)
-            wrongMessage = "Unrecognized parking ticket.";
+            wrongMessage = wrongMessages.get("unRecognize");
         return car;
     }
 }
