52a8dc5459c6a85336c960980c5dddf6310b2c7c
Lichuwen <lichuwen@CHUWENdeMacBook-Pro.local>
2020-07-26 08:18:19 +0800

    [story2][karen] test:should_return_wrong_message_when_park_given_no_position

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -52,4 +52,22 @@ public class ParkingBoyTest {
         //then
         assertEquals("Please provide your parking ticket.", wrongMessage);
     }
+
+    @Test
+    void should_return_wrong_message_when_park_given_no_position(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        for(int i=0; i<10; i++){
+            parkingBoy.park(new Car());
+        }
+        Car car = new Car();
+        CarTicket carTicket = parkingBoy.park(car);
+
+        //when
+        String wrongMessage = parkingBoy.getWrongMessage();
+
+        //then
+        assertEquals("Not enough position.", wrongMessage);
+    }
 }
