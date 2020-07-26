5c7c12ac36a90f4e19f6b63dda240daf6be8a347
lichuwen <545759585@qq.com>
2020-07-26 16:12:14 +0800

    [story4][karen] feature:add SmartParkingBoy's chooseParkingLot

---
+++ src/main/java/com/oocl/cultivation/test/SmartParkingBoy.java
@@ -0,0 +1,24 @@
+package com.oocl.cultivation.test;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class SmartParkingBoy extends ParkingBoy{
+
+    public SmartParkingBoy(ArrayList<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+
+    @Override
+    public ParkingLot chooseParkingLot(ArrayList<ParkingLot> parkingLots) {
+        ParkingLot suitParkingLot = parkingLots.get(0);
+        int largestEmptyPosition = 0;
+        for(ParkingLot parkingLot:parkingLots){
+            if(parkingLot.getEmptyPosition() > largestEmptyPosition){
+                suitParkingLot = parkingLot;
+                largestEmptyPosition = parkingLot.getEmptyPosition();
+            }
+        }
+        return suitParkingLot;
+    }
+}
