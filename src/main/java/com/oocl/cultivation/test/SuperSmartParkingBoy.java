a8a0fc81a9501679ef8aaa63b32a72f49c536da9
lichuwen <545759585@qq.com>
2020-07-26 17:16:53 +0800

    [story5][karen] feature:SuperSmartParkingBoy's chooseParkingLot function

---
+++ src/main/java/com/oocl/cultivation/test/SuperSmartParkingBoy.java
@@ -0,0 +1,24 @@
+package com.oocl.cultivation.test;
+
+import java.util.ArrayList;
+
+public class SuperSmartParkingBoy extends ParkingBoy{
+
+    public SuperSmartParkingBoy(ArrayList<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+
+    @Override
+    public ParkingLot chooseParkingLot(ArrayList<ParkingLot> parkingLots) {
+        ParkingLot suitParkingLot = parkingLots.get(0);
+        double highRateLot = 0,parkingLotRate;
+        for(ParkingLot parkingLot:parkingLots){
+            parkingLotRate = parkingLot.getEmptyPosition() * 1.0 / parkingLot.getCapacity();
+            if(parkingLotRate > highRateLot){
+                suitParkingLot = parkingLot;
+                highRateLot = parkingLotRate;
+            }
+        }
+        return suitParkingLot;
+    }
+}
