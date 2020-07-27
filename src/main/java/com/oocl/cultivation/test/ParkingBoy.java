776478e8754a3362e9f5ea98b81ab6be3f249ca6
lichuwen <545759585@qq.com>
2020-07-27 16:59:06 +0800

    [karen] refactor:refresh ArrayList<ParkingLot>'s access protected to private

--- src/main/java/com/oocl/cultivation/test/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/test/ParkingBoy.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class ParkingBoy implements bestChoiceOfParkingLots {
 
-    ArrayList<ParkingLot> parkingLots;
+    private ArrayList<ParkingLot> parkingLots;
     private String wrongMessage;
     private Map<String, String> wrongMessages = new HashMap<>();
 
